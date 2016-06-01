b5e5d71854b6927c0fdf457edf4e1b142d4291d8
Elliot Hesp <elliot.hesp@gmail.com>
2016-06-01 10:53:54 +0100

    Only run schedules if its a valid array with items

--- src/hook.js
+++ src/hook.js
@@ -12,6 +12,10 @@ export default class Scheduler extends BaseHook {
    * @returns {Promise.<T>}
    */
   initialize() {
+    if (!this.options.schedules || !this.options.schedules.length) {
+      return Promise.resolve();
+    }
+    
     for (let i = 0, len = this.options.schedules.length; i < len; i++) {
       const schedule = this.options.schedules[i];
       this.options.laterSchedules[i] = later.parse.text(schedule.interval);
