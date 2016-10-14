7d95cac9eeb547f8ab591fe94408787927232115
salakar <mike.diarmid@gmail.com>
2016-10-14 14:48:57 +0100

    mocha setup spec now dynamically creates clustered redis tests for tests starting with '...'

--- test/setup.spec.js
+++ test/setup.spec.js
@@ -2,12 +2,34 @@ global.HOOK_NAME = 'schedule';
 const Redibox = require('redibox').default;
 const UserHook = require('./../src/hook');
 
-const config = {
+
+global.defaultScheduleTest = function defaultScheduleTest(...args) {
+  if (global.defaultScheduleRunner) return global.defaultScheduleRunner.call(this, ...args);
+  return Promise.resolve();
+};
+
+
+const configBase = {
   hooks: {},
   schedule: {
     enabled: true,
-    schedules: [],
+    schedules: [
+      {
+        name: 'everySecDefaultFunc',
+        runs: 'defaultScheduleTest',
+        interval: 'every 1 seconds',
+      },
+    ],
   },
+  pubsub: {
+    publisher: true,
+    subscriber: true,
+  },
+  // log: { level: 'verbose' },
+};
+
+const config = Object.assign({}, configBase, {});
+const configCluster = Object.assign({}, configBase, {
   redis: {
     connectionTimeout: 2000,
     hosts: [
@@ -37,33 +59,45 @@ const config = {
       },
     ],
   },
-  pubsub: {
-    publisher: true,
-    subscriber: true,
-  },
-};
+});
 
 config.hooks[global.HOOK_NAME] = UserHook;
+configCluster.hooks[global.HOOK_NAME] = UserHook;
 
 before((done) => {
   global.RediBox = new Redibox(config, () => {
-    global.Hook = RediBox.hooks[global.HOOK_NAME];
-    if (config.redis && config.redis.hosts) {
-      RediBox.cluster.flushall().then(() => done());
-    } else {
-      RediBox.client.flushall().then(() => done());
-    }
+    global.Hook = global.RediBox.hooks[global.HOOK_NAME];
+    global.RediBoxClustered = new Redibox(configCluster, () => {
+      global.HookClustered = global.RediBoxClustered.hooks[global.HOOK_NAME];
+      setTimeout(done, 1000);
+    });
   });
 });
 
-beforeEach((done) => {
-  if (config.redis && config.redis.hosts) {
-    RediBox.cluster.flushall().then(() => done());
-  } else {
-    RediBox.client.flushall().then(() => done());
+beforeEach(function beforeEach(done) {
+  if (this.currentTest.title.startsWith('CLUSTERED - ')) {
+    this.currentTest.hook = global.HookClustered;
+    RediBoxClustered.cluster.flushall().then(() => done());
+    return null;
   }
+
+  this.currentTest.hook = global.Hook;
+
+  if (this.currentTest.title.startsWith('...')) {
+    const newTitle = this.currentTest.title.replace('...', 'CLUSTERED -');
+    this.currentTest.title = this.currentTest.title.replace('...', 'SINGLE -');
+    const fn = this.currentTest.fn;
+    describe(newTitle, function () {
+      it(newTitle, fn);
+    });
+    RediBox.client.flushall().then(() => done());
+    return null;
+  }
+
+  return done();
 });
 
 after(() => {
   RediBox.disconnect();
+  RediBoxClustered.disconnect();
 });
